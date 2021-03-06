521dc0de8ae379d70ef53904e686821c52bd92e0
Mert Sonmez <mertsonmez93@outlook.com>
2021-03-07 00:27:17 +0300

    Angular added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+//c# daki using gibidir
 import { Component } from '@angular/core';
 
+//süslü parantez demek javascriptte object demek!!
+//bir component basit bir class iken 
 @Component({
+  //app-root u tanıttığımız yer --> beni app-routre olarak index.html de kullanabilirsin demek !!
   selector: 'app-root',
+  //app.component.html in datasını yönetecek olan komponentsin demek templete url
+  // ./ aynı klasör demek
   templateUrl: './app.component.html',
+  //bu html in css lerini koyduğumuz yer 
+  //[ ] javascript dünyasında array demek yani birden fazla css iniz olabilir virgülle ayırabilirsiniz 
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwind';
+  //title:string dersek tip güvenli hale getirebiliriz !!
+  title: string = 'northwind';
+  user: string = "Engin Demirog";
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//angular cli module e importlarıma ekledi
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //ve module de bu componentleri kullanacağım diyerek ekledi !!
+    ProductComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
