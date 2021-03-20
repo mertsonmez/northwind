ac4e05c0bf276df3aa7fee747e0a9c61ab1cf2e3
Mert Sonmez <mertsonmez93@outlook.com>
2021-03-20 22:45:00 +0300

    ProductAdd operations written

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 //form Builder reactive formun service i bizim bir form html ile burada yazacağımız kontrolleri ilişkilendiriğimiz nokta
 //form builder servisi html deki form ile burayı yapılandırmamızı sağlıyor 
@@ -15,10 +17,11 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm: FormGroup;
   
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private productService : ProductService , private toastrService: ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
+    this.add();
   }
 
   createProductAddForm(){
@@ -31,7 +34,21 @@ export class ProductAddComponent implements OnInit {
   }
 
   add(){
-    
+    //ekleme işlemi yapabilmek için form valid mi bilmemiz gerek
+    if (this.productAddForm.valid){
+      let productModel = Object.assign({},this.productAddForm.value) //product model için bir object oluşturuyor içi boş ve buradaki alalnların hepsini bu objenin içine ekleyecek.
+      this.productService.add(productModel).subscribe(response => {
+        console.log(response)
+        this.toastrService.success(response.message,"Başarılı")
+      }, responseError =>{
+        console.log(responseError.error)
+        this.toastrService.error(responseError.error)
+      })
+    }
+    else{
+      this.toastrService.error("Formunuz Eksik","Dikkat; Ürün Eklenemedi")
+    }
+
   }
 
   // reactive formlar için forms module olması gerekiyor
